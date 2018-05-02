cc2e0ffccf2c00abfa0b08ce4f4307f51cc11803
张梦洲 <1421135093@qq.com>
2018-05-02 21:42:27 +0800

    Update spider_main.py

--- spider_main.py
+++ spider_main.py
@@ -20,7 +20,7 @@ class SpiderMain(object):
                 self.urls.add_new_urls(new_urls)
                 self.outputer.collect_data(new_data)
 
-                if count == 10:
+                if count == 1000:
                     break
 
                 count = count + 1
